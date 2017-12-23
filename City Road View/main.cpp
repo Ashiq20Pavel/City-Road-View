7b97a0a42be79cf80d8e689964bdd8535cba82db
Jame <jameahmed@github.com>
2017-12-24 00:03:20 +0600

    Check

--- City Road View/main.cpp
+++ City Road View/main.cpp
@@ -726,7 +726,6 @@ void SchoolDoor()
 
 }
 
-
 void Sun()
 {
         glColor3ub(246,171,26);
@@ -736,26 +735,6 @@ void Sun()
 
 void Cloud()
 {
-<<<<<<< HEAD
-
-        glColor3ub(230,234,237);
-        drawCircle(594.0f,586.0f,20.0f);
-        drawCircle(572.0f,595.0f,28.0f);
-        drawCircle(539.0f,595.0f,35.0f);
-        drawCircle(513.0f,575.0f,20.0f);
-
-        drawCircle(393.0f,577.0f,20.0f);
-        drawCircle(370.0f,585.0f,28.0f);
-        drawCircle(339.0f,583.0f,35.0f);
-        drawCircle(311.0f,565.0f,20.0f);
-
-        drawCircle(193.0f,607.0f,20.0f);
-        drawCircle(170.0f,615.0f,28.0f);
-        drawCircle(139.0f,613.0f,35.0f);
-        drawCircle(111.0f,595.0f,20.0f);
-
-        //glutPostRedisplay();
-=======
     glColor3ub(230,234,237);
     drawCircle(594.0f,586.0f,20.0f);
     drawCircle(572.0f,595.0f,28.0f);
@@ -771,7 +750,6 @@ void Cloud()
     drawCircle(170.0f,615.0f,28.0f);
     drawCircle(139.0f,613.0f,35.0f);
     drawCircle(111.0f,595.0f,20.0f);
->>>>>>> f93add8c986bb17050f7ed46b85a49289c349f50
 }
 
 
@@ -969,7 +947,7 @@ void Tree()
 
 void LampPost()
 {
-    for(int i=0;i<8*115;i+=115)
+    for(int i=0;i<8*11;i+=115)
     {
         glPushMatrix();
         glColor3ub(0,0,0);
@@ -1075,29 +1053,13 @@ void Car1()
 
     //glPushMatrix();
     glColor3ub(48,48,46);
-<<<<<<< HEAD
-    drawCircle(342-500,116,15);
-    glColor3ub(255,255,255);
-    drawCircle(342-500,116,10);
-    glColor3ub(0,0,0);
-    drawCircle(342-500,116,8);
-=======
     drawCircle(342-500,110,15);
->>>>>>> f93add8c986bb17050f7ed46b85a49289c349f50
     //glPopMatrix();
 
     //glPushMatrix();
     glColor3ub(48,48,46);
-<<<<<<< HEAD
-    drawCircle(434-500,116,15);
-    glColor3ub(255,255,255);
-    drawCircle(434-500,116,10);
-    glColor3ub(0,0,0);
-    drawCircle(434-500,116,8);
-=======
     drawCircle(434-500,110,15);
 
->>>>>>> f93add8c986bb17050f7ed46b85a49289c349f50
     glPopMatrix();
 }
 
@@ -1163,12 +1125,8 @@ void myDisplay(void)
 
         Sun();
         Cloud();
-<<<<<<< HEAD
-        RoadLight();
-=======
 
         BetweenRoadAndBuldings();
->>>>>>> f93add8c986bb17050f7ed46b85a49289c349f50
         Road();
         LampPost();
         RoadCorner1();
